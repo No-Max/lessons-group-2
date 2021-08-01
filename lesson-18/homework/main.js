f06ca97d4702f26113e8391bb9170fd0b30b0c29
No-Max <malyavko.max@gmail.com>
2021-08-01 20:54:02 +0300

    add homework-18 initial code

---
+++ lesson-18/homework/main.js
@@ -0,0 +1,46 @@
+const cities = [
+  // массив нашик опшенов для селекта
+  {
+    name: "Москва",
+    timeZone: "Europe/Moscow",
+  },
+  {
+    name: "Лондон",
+    timeZone: "Europe/London",
+  },
+  {
+    name: "Токио",
+    timeZone: "Asia/Tokyo",
+  },
+  {
+    name: "Пекин",
+    timeZone: "Asia/Shanghai",
+  },
+];
+
+/**
+ * getDateAndTimeByTimeZone возвращает дату и в ремя для переданной таймзоны
+ * @param {string} tz, таймзона https://www.iplocate.com/en/resources/timezones
+ * @returns {object}, объект с датой и временем, пример:  {date: "2 августа", time: "01:45:41"}
+ */
+function getDateAndTimeByTimeZone(tz = "Europe/Moscow") {
+  const date = new Date();
+
+  return {
+    date: date.toLocaleDateString("ru", {
+      month: "long",
+      day: "numeric",
+      timeZone: tz,
+    }),
+    time: date.toLocaleTimeString("ru", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      timeZone: tz,
+    }),
+  };
+}
+
+cities.forEach((city) => {
+  console.log(getDateAndTimeByTimeZone(city.timeZone)); // вывод времени и даты для каждой зоны
+});
