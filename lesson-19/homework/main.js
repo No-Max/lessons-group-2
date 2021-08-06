31e64d03f5a837f1e101d181e8c6e9eb4a90c2b6
No-Max <malyavko.max@gmail.com>
2021-08-06 15:37:27 +0300

    roll back homework

--- lesson-19/homework/main.js
+++ lesson-19/homework/main.js
@@ -12,42 +12,6 @@ const items = [
   "Печенье",
 ];
 
-class Search {
-  _value = "";
-  constructor(selector, onInput) {
-    this.$element = document.querySelector(selector);
-    this.$element.addEventListener("input", ({ target }) => {
-      this._value = target.value;
-      onInput(this._value.toLowerCase());
-    });
-  }
-}
+class Search {}
 
-class Results {
-  constructor(selector, items = []) {
-    this.$element = document.querySelector(selector);
-    this._items = items;
-    this._searchResults = items;
-    this.render(); // довольно опасно вызывать методы в конструкторе, но если вы уверены в своем коде, то можно
-  }
-
-  searchItems(text) {
-    this._searchResults = this._items.filter((item) =>
-      item.toLowerCase().includes(text)
-    );
-    this.render();
-  }
-
-  render() {
-    this.$element.innerHTML = this._searchResults.reduce(
-      (result, item) => (result += `<li>${item}</li>`),
-      ""
-    );
-  }
-}
-
-const results = new Results("#results", items);
-
-const search = new Search("#search", (text) => {
-  results.searchItems(text);
-});
+class Results {}
