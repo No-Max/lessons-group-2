4b815e226e327881f0ab42e2f48be7a14aaa433d
No-Max <malyavko.max@gmail.com>
2021-08-06 14:31:04 +0300

    add homework-19 app classes code

--- lesson-19/homework/main.js
+++ lesson-19/homework/main.js
@@ -12,6 +12,40 @@ const items = [
   "Печенье",
 ];
 
-class Search {}
+class Search {
+  constructor(selector, onInput) {
+    this.$element = document.querySelector(selector);
+    this.$element.addEventListener("input", ({ target }) =>
+      onInput(target.value.toLowerCase())
+    );
+  }
+}
 
-class Results {}
+class Results {
+  constructor(selector, items = []) {
+    this.$element = document.querySelector(selector);
+    this._items = items;
+    this._searchResults = items;
+    this.render(); // довольно опасно вызывать методы в конструкторе, но если вы уверены в своем коде, то можно
+  }
+
+  searchItems(text) {
+    this._searchResults = this._items.filter((item) =>
+      item.toLowerCase().includes(text)
+    );
+    this.render();
+  }
+
+  render() {
+    this.$element.innerHTML = this._searchResults.reduce(
+      (result, item) => (result += `<li>${item}</li>`),
+      ""
+    );
+  }
+}
+
+const results = new Results("#results", items);
+
+const search = new Search("#search", (text) => {
+  results.searchItems(text);
+});
