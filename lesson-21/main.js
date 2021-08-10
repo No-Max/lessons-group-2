a3582753954b4c31893165c47470bcb8279a80f6
No-Max <malyavko.max@gmail.com>
2021-08-10 12:41:16 +0300

    add code from lesson 21

---
+++ lesson-21/main.js
@@ -0,0 +1,23 @@
+// document.body.addEventListener("click", (event) => {
+//   console.log("2");
+// });
+
+// document.querySelector(".second").addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   console.log("1");
+// });
+
+// console.log(1);
+// try {
+//   new Map({});
+// } catch (err) {
+//   alert(err);
+// }
+
+// console.log(2);
+
+// console.log(1);
+// let error = new SyntaxError("Моя ошибка");
+// throw error;
+
+// console.log(2);
