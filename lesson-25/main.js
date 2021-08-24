ba02e40b343a50a1498982316d698a53e7f03a78
No-Max <malyavko.max@gmail.com>
2021-08-24 17:44:44 +0300

    add code from lesson 25

---
+++ lesson-25/main.js
@@ -0,0 +1,25 @@
+// class Component {
+//   _propert;
+//   constructor(name) {}
+//   method() {}
+//   get property() {}
+//   set property(value) {}
+//   static render() {}
+// }
+
+// const exemple1 = new Component("Sam");
+// const exemple2 = new Component("Bobby");
+
+//http://127.0.0.1:5500/?test=dasdasasdas
+
+// const form = document.forms.form1;
+// form.test.focus();
+// form.test.addEventListener("input", () => {
+//   if (form.test.value.length > 10) {
+//     form.test.blur();
+//   }
+// });
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   console.log(form1.elements);
+// });
