870a6286aaa999d4c0a0a9e8fad36636e51e1f66
No-Max <malyavko.max@gmail.com>
2021-08-29 13:19:27 +0300

    add code from lesson-26

---
+++ lesson-26/main.js
@@ -0,0 +1,92 @@
+// const obj1 = {
+//   name: 'Max',
+//   sayHi: function () {
+//     console.log("Hi");
+//   },
+// };
+
+// const obj2 = {
+//   name: "Max",
+//   sayHi: function () {
+//     console.log("Hi");
+//   },
+// };
+
+// const obj3 = {
+//   name: "Max",
+//   sayHi: function () {
+//     console.log("Hi");
+//   },
+// };
+
+// function createObj(name) {
+//   return {
+//     name: name,
+//     sayHi: function () {
+//       console.log("Hi");
+//     },
+//   };
+// }
+
+// const obj1 = createObj("max1");
+// const obj2 = createObj("max2");
+// const obj3 = createObj("max3");
+
+// function Obj(name) {
+//   this.name = name;
+//   this.sayHi = function () {
+//     console.log("Hi");
+//   };
+// }
+
+// const obj1 = new Obj("max");
+// obj1.sayHi();
+
+// class Obj {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   sayHi() {
+//     console.log("Hi");
+//   }
+// }
+
+// const obj1 = new Obj("max1");
+// const obj2 = new Obj("max2");
+// const obj3 = new Obj("max3");
+
+// sessionStorage.setItem("color", "red");
+
+// function f1(a, b, c) {
+//   console.log(a, b, c);
+// }
+
+// function f2(type) {
+//   return function (b) {
+//     return function (c) {
+//       console.log(a, b, c);
+//     };
+//   };
+// }
+
+// f1(1, 2, 3);
+// f1(1, 2, 4);
+// f1(1, 2, 5);
+
+// const f3 = f2('wrn');
+// const f4 = f2("errr");
+// const f5 = f2("succes");
+// f3(3)(1); //wrn 31
+// f3(2)(10); //
+
+// f3(4)();
+// f3(5)();
+
+//const a = 'a';
+//const str = `https://domain.com/${page}`;
+
+// const { a, ...rest } = { a: "a", b: "b", c: "c" };
+
+// let a = ['a', 'b']
+
+// function f(a) { }
